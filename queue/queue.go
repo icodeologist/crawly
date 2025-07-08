6aa1ebe0841273595cdca34935c64b5f860276f3
icodeologist <denzilpinto01@gmail.com>
2025-07-08 07:43:02 +0530

    Simple FIFO queue Data structure

---
+++ queue/queue.go
@@ -0,0 +1,30 @@
+package queue
+
+type Queue struct {
+	items []string
+}
+
+func New() *Queue {
+	return &Queue{items: []string{}}
+}
+
+func (q *Queue) Enqueue(item string) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() (string, bool) {
+	if len(q.items) == 0 {
+		return "", false
+	}
+	item := q.items[0]
+	q.items = q.items[1:]
+	return item, true
+}
+
+func (q *Queue) IsEmpty() bool {
+	return len(q.items) == 0
+}
+
+func (q *Queue) Len() int {
+	return len(q.items)
+}
